54fac5cfac3e476e30c59a85ef2c5cbd3813cf72
sofiailse <sofiailsefp@gmail.com>
2025-04-13 13:19:49 +0200

    Fix some logo styles and add my account to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../resources/logo.png';
+import { MdAccountCircle } from 'react-icons/md';
+import { useNavigate } from 'react-router';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="header">
-      <img src={logo} alt="Logo" className="logo" />
+      <a href="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </a>
+
+      <MdAccountCircle size={37} onClick={() => navigate('/myAccount')} />
     </div>
   );
 };
