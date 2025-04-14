801ca25847f932f6d8e4cfa8e9a1c66bf76c16c3
sofiailse <sofiailsefp@gmail.com>
2025-04-14 20:15:35 +0200

    Update dependencies and integrate React Query Devtools for enhanced data fetching capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,51 @@ import LibraryProvider from './context/LibraryProvider';
 import MyAccount from './pages/myAccount/MyAccount';
 import MainApp from './layouts/MainApp';
 import ErrorBoundary from './components/ui/error/ErrorBoundary';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+// Create a client
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 
 const App = () => {
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
   return (
     <>
-      <ErrorBoundary>
-        <LibraryProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<MainApp />}>
-                <Route index element={<BooksList />} />
-                <Route path="/myAccount" element={<MyAccount />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </LibraryProvider>
-      </ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary>
+          <LibraryProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route element={<MainApp />}>
+                  <Route index element={<BooksList />} />
+                  <Route path="/myAccount" element={<MyAccount />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </LibraryProvider>
+        </ErrorBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+        {showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )}
+      </QueryClientProvider>
     </>
   );
 };
