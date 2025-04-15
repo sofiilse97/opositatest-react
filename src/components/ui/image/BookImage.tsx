8a1cdac2d7b85b5147f01219ac6b0e5d0365e31f
sofiilse97 <74297701+sofiilse97@users.noreply.github.com>
2025-04-15 13:40:12 +0200

    Create book image component

---
+++ src/components/ui/image/BookImage.tsx
@@ -0,0 +1,15 @@
+import defaultBook from '../../../resources/defaultBook.png';
+
+const BookImage = ({src}: {src: string}) => {
+
+  return (
+    <img src={src}
+      style={{
+        backgroundImage: `url(${defaultBook})`,
+      }} 
+    />
+  )
+
+}
+
+export default BookImage;
