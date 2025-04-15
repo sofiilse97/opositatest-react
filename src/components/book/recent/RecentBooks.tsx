b46c2c60689ec99c69f3e1ad3a073f385b86d2ba
sofiailse <sofiailsefp@gmail.com>
2025-04-15 18:49:49 +0200

    Change styles

--- src/components/book/recent/RecentBooks.tsx
+++ src/components/book/recent/RecentBooks.tsx
@@ -1,5 +1,8 @@
 import { BookType } from '@/types/book';
+import BookImage from '@/components/ui/image/BookImage';
 
+import './recentBooks.css';
+import { getCoverFile } from '@/api/constants/books';
 const RecentBooks = ({
   recentBooks,
   books,
@@ -12,23 +15,18 @@ const RecentBooks = ({
   return (
     <div style={{ width: '100%' }}>
       <h3>Recientes</h3>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '10px',
-          overflowY: 'auto',
-        }}
-      >
+      <div className="recent-books-container">
         {Array.from(recentBooks).map((url) => {
           const book = books.find((b) => b.url === url);
           return book ? (
-            <button
-              style={{ minWidth: '180px' }}
+            <div
+              className="recent-book"
+              key={`recents-${book.url}`}
               onClick={() => handleBook(book)}
             >
-              {book.name}
-            </button>
+              <BookImage src={getCoverFile({ isbn: book.isbn })} />
+              <p>{book.name}</p>
+            </div>
           ) : null;
         })}
       </div>
