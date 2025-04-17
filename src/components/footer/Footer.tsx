bab8e216719ec8367f21824642cdb23d23456764
sofiailse <sofiailsefp@gmail.com>
2025-04-17 12:54:02 +0200

    Fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,13 @@
 import './footer.css';
 
 const Footer = () => {
-  return <div className="footer">Prueba técnica OpositaTest</div>;
+  return (
+    <div className="footer">
+      <p> Prueba técnica OpositaTest, </p>
+
+      <a href="https://github.com/sofiilse97/opositatest-react">Sofía Ilse</a>
+    </div>
+  );
 };
 
 export default Footer;
