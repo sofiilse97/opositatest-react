da30827198f7e24ec1b8f95944ca06a112361889
sofiailse <sofiailsefp@gmail.com>
2025-05-20 20:37:14 +0200

    Remove unnecesary props use context instead

--- src/components/book/recent/RecentBooks.tsx
+++ src/components/book/recent/RecentBooks.tsx
@@ -3,31 +3,41 @@ import BookImage from '@/components/ui/image/BookImage';
 
 import './recentBooks.css';
 import { getCoverFile } from '@/api/search/books';
+import { useLibrary } from '@/context/hooks/useLibrary';
 
-const RecentBooks = ({
-  recentBooks,
-  handleBook,
-}: {
-  recentBooks: Map<string, BookType>;
+const RecentBooks = () => {
+  const { libraryState, setLibraryState } = useLibrary();
 
-  handleBook: (bk: BookType) => void;
-}) => {
+  const handleBook = (bk: BookType) => {
+    const newRecents = new Map(libraryState.recentBooks || []);
+    newRecents.set(bk.url, bk);
+
+    setLibraryState({
+      selectedBook: bk,
+      recentBooks: newRecents,
+    });
+  };
+
   return (
-    <div style={{ width: '100%' }}>
-      <h3>Recientes</h3>
-      <div className="recent-books-container">
-        {Array.from(recentBooks.entries()).map(([url, book]) => (
-          <div
-            className="recent-book"
-            key={`recents-${url}`}
-            onClick={() => handleBook(book)}
-          >
-            <BookImage src={getCoverFile({ isbn: book.isbn })} />
-            <p>{book.name}</p>
-          </div>
-        ))}
+    libraryState.recentBooks.size > 0 && (
+      <div style={{ width: '100%' }}>
+        <h3>Recientes</h3>
+        <div className="recent-books-container">
+          {Array.from(
+            libraryState.recentBooks.entries() as [string, BookType][]
+          ).map(([url, book]) => (
+            <div
+              className="recent-book"
+              key={`recents-${url}`}
+              onClick={() => handleBook(book)}
+            >
+              <BookImage src={getCoverFile({ isbn: book.isbn })} />
+              <p>{book.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
